1cf6f7b9168bc859f6b6d0bc0cecb88482f9333b
Weijing Jay Lin <weijingjaylin@gmail.com>
2022-03-06 17:55:25 -0800

    Update plans.js

--- assets/js/plans.js
+++ assets/js/plans.js
@@ -3,7 +3,6 @@ const att = {
   getUnlimitedPlanPrice: (numUsers) => {
     console.log("att", numUsers);
     switch (parseInt(numUsers)) {
-      
       case 4:
         return 35 * parseInt(numUsers);
       case 3:
@@ -15,7 +14,7 @@ const att = {
       default:
         return 30 * parseInt(numUsers);
     }
-  }
+  },
 };
 
 const tMobile = {
@@ -34,7 +33,7 @@ const tMobile = {
       default:
         return 24 * numUsers;
     }
-  }
+  },
 };
 
 const tello = {
@@ -44,81 +43,81 @@ const tello = {
   },
   lowestUnitPrice: 39,
   dataUnlimite: true,
-  website: "https://tello.com/buy/family_plans"
+  website: "https://tello.com/buy/family_plans",
 };
 
 const customerCellular = {
   label: "Customer Cellular",
-    getUnlimitedPlanPrice: (numUsers) => {
-      numUsers = parseInt(numUsers);
-      switch (numUsers) {
-        case 1:
-          return 55;
-        case 2:
-          return 75;
-        case 3:
-          return 90;
-        default:
-          return 30 * (numUsers - 2) + 25 * (numUsers - 3) || 0;
-      }
-    },
-    dataUnlimited: true,
-    website: "https://www.consumercellular.com/shopping/choose/plan"
-  };
+  getUnlimitedPlanPrice: (numUsers) => {
+    numUsers = parseInt(numUsers);
+    switch (numUsers) {
+      case 1:
+        return 55;
+      case 2:
+        return 75;
+      case 3:
+        return 90;
+      default:
+        return 30 * (numUsers - 2) + 25 * (numUsers - 3) || 0;
+    }
+  },
+  dataUnlimited: true,
+  website: "https://www.consumercellular.com/shopping/choose/plan",
+};
 
 const visible = {
   label: "Visible",
-    network: "Verizon",
-    price: 25.0,
-    dataUnlimited: true,
-    getUnlimitedPlanPrice: (num) => 25 * parseInt(num),
-    comment: `
-      For party plan/multiple users plan, 
-      it requires at least one user to be the member first and then
-      create the party plan.`,
-    website: "https://www.visible.com/"
-  };
+  network: "Verizon",
+  price: 25.0,
+  dataUnlimited: true,
+  getUnlimitedPlanPrice: (num) => 25 * parseInt(num),
+  comment: `
+        For party plan/multiple users plan,
+        it requires at least one user to be the member first and then
+        create the party plan.`,
+  website: "https://www.visible.com/",
+};
 
 const usMobile = {
   label: "US Mobile",
-    dataUnlimited: true,
-    getUnlimitedPlanPrice: (num) => {
-      switch (num) {
-        case 1:
-          return 45;
-        case 2:
-          return 30;
-        default:
-          return 25 * num;
-      }
-    },
-    getPriceByData: (numGb, numUser) => {
-      switch (numGb) {
-        case 0:
-          return [8, 7, 6][numUser - 1];
-        case 1:
-          return numUser * 12;
-        case 5:
-          return numUser * 15;
-        case 12:
-          return numUser * 20;
-        case 18:
-          return numUser * 25;
-        case 30:
-          return numUser * 30;
-        default:
-          return "Unkown: no matching data plan was found";
-      }
+  dataUnlimited: true,
+  getUnlimitedPlanPrice: (num) => {
+    switch (num) {
+      case 1:
+        return 45;
+      case 2:
+        return 30;
+      default:
+        return 25 * num;
     }
-  };
+  },
+  getPriceByData: (numGb, numUser) => {
+    switch (numGb) {
+      case 0:
+        return [8, 7, 6][numUser - 1];
+      case 1:
+        return numUser * 12;
+      case 5:
+        return numUser * 15;
+      case 12:
+        return numUser * 20;
+      case 18:
+        return numUser * 25;
+      case 30:
+        return numUser * 30;
+      default:
+        return "Unkown: no matching data plan was found";
+    }
+  },
+};
 
 const mintMobile = {
   label: "Mint Mobile",
-    getUnlimitedPlanPrice: (numUsers) => numUsers * 30,
-    lowestUnitPrice: 30,
-    dataUnlimted: true,
-    website: "https://www.mintmobile.com/plans/"
-  };
+  getUnlimitedPlanPrice: (numUsers) => numUsers * 30,
+  lowestUnitPrice: 30,
+  dataUnlimted: true,
+  website: "https://www.mintmobile.com/plans/",
+};
 
 const plans = {
   att,
@@ -129,3 +128,5 @@ const plans = {
   mintMobile,
   usMobile,
 };
+
+export default plans;
